8e60f01febb843bc77f75df0105ed1dc0089a518
Tronicality <theoameku@gmail.com>
2023-11-22 03:42:56 +0000

    Removed Update Point
    Wasn't working

--- script/Pack.js
+++ script/Pack.js
@@ -1,3 +1,41 @@
+// ==UserScript==
+// @name         Br1h's Pack
+// @version      1.0.2
+// @description  For evades sandbox, ability to hide card or minimap or time and submit to a leaderboard.
+// @author       Br1h
+// @match        https://ravel.turudu.repl.co/
+// @match        https://ravel-beta.sonicexe66.repl.co/
+// @require      https://code.jquery.com/jquery-3.6.4.min.js
+// @require      https://raw.githubusercontent.com/Tronicality/Time-Attack/main/script/config.js
+// @require      https://raw.githubusercontent.com/Tronicality/Time-Attack/main/script/html.js
+// @require      https://raw.githubusercontent.com/Tronicality/Time-Attack/main/script/Client%20Helper.js
+// @require      https://raw.githubusercontent.com/Tronicality/Time-Attack/main/script/Pack.js
+// @downloadURL  https://raw.githubusercontent.com/Tronicality/Time-Attack/main/script/Pack.js
+// @updateURL    https://raw.githubusercontent.com/Tronicality/Time-Attack/main/script/Pack.js
+// @run-at       document-end
+// @icon         https://www.google.com/s2/favicons?domain=evades.io
+// @grant        none
+// ==/UserScript==
+
+/* Ideas
+ - Allow player to get more data than just the top 10 leaderboard info for example allTopPlayers (all collections)
+ - Allow 1 area skip mode
+ - Community hero ranking filter(everything, solo, duo, map, top 10 players)
+*/
+
+//TODO: update
+//TODO: documentation
+//TODO: Cheat
+//TODO: data
+//TODO: server
+//TODO: rendering
+//TODO: leaderboard
+//TODO: accounts
+//TODO: beg to either: Kaluub, sonicexe, ravel, DD1
+
+//TODO: leaderboard - if player not in top 10, still show their rank and time info
+//TODO: update - bug where user has to make a new userscript for update to come, hopeing changing the version fixes this
+
 window.renderArea = renderBr1hArea;
 
 function handleKey(event) {
